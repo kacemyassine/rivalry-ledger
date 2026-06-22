2ed83afac193891f36831e1d68df1df84c769bd1
kacem yassine <kacemyassine4@gmail.com>
2026-06-22 17:34:50 +0100

    chore: add Cypress + Cucumber e2e testing setup

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    specPattern: "cypress/e2e/**/*.feature", 
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config; 
+    },
+  },
+});
