6c718aff4d17f4f4ce6f48b284d32c8818c4c703
kacem yassine <kacemyassine4@gmail.com>
2026-06-28 13:43:56 +0100

    fix(toast): toast unclickable while dialog open due to body pointer-events lock

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      style={{ pointerEvents: "auto" } as React.CSSProperties} 
       toastOptions={{
         classNames: {
           toast:
