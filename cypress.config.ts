164206562cff74e7f9f80dd8c1477c46e15df8d6
kacem yassine <kacemyassine4@gmail.com>
2026-06-28 20:53:47 +0100

    chore(cypress): add __dirname polyfill for ESM compatibility in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,11 @@ import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   allowCypressEnv: true,
