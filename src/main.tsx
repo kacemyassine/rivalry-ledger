d2d0cef34e717ce2cb32f2845dba436123787f05
kacem yassine <kacemyassine4@gmail.com>
2026-07-06 21:09:53 +0100

    Revert "fix(debug): add JSON.parse wrapper to trace parse errors"
    This reverts commit 368d99e4bd6215f237c4a140457a9fafa0f32803.

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const _parse = JSON.parse;
-JSON.parse = function(str, ...args) {
-  try {
-    return _parse.call(this, str, ...args);
-  } catch (e) {
-    console.error("JSON.parse failed on:", str);
-    throw e;
-  }
-};
-
 createRoot(document.getElementById("root")!).render(<App />);
