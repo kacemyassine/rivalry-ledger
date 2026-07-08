baf3b85e576ad9b7df3f555b5dd154acd968e490
kacem yassine <kacemyassine4@gmail.com>
2026-07-08 16:26:16 +0100

    test(e2e): add HEAD intercept for github connectivity check

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,6 +3,7 @@ import "./commands";
 beforeEach(() => {
   cy.fixture("leagueData.json").then((data) => {
     const encoded = btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+    cy.intercept("HEAD", "https://api.github.com", { statusCode: 200}).as("checkOnline");
     cy.intercept("GET", "https://api.github.com/**", {
       body: { content: encoded, sha: "abc123" },
     }).as("getLeagueData");
